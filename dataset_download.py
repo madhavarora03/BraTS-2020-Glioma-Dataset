dd7783d53e8f47dc34ffcc0679895768eedfe297
madhav <eragon.cube@outlook.com>
2025-02-19 06:43:49 +0530

    Refactor dataset directory structure and update .gitignore file.

--- dataset_download.py
+++ dataset_download.py
@@ -12,16 +12,20 @@ path = kagglehub.dataset_download("awsaf49/brats20-dataset-training-validation")
 print(f"{Fore.GREEN}Path to dataset files:{Style.RESET_ALL} {path}")
 
 # Check if 'data' directory exists, if not, copy the dataset
-if not os.path.exists("data"):
+if not os.path.exists("data/training_data"):
     try:
-        shutil.copytree(f"{path}/BraTS2020_TrainingData/MICCAI_BraTS2020_TrainingData", "data/")
-        print(f"{Fore.CYAN}Dataset copied to 'data/' directory.{Style.RESET_ALL}")
+        shutil.copytree(f"{path}/BraTS2020_TrainingData/MICCAI_BraTS2020_TrainingData", "data/training_data/")
+        print(f"{Fore.CYAN}Dataset copied to 'data/training_data' directory.{Style.RESET_ALL}")
     except Exception as e:
         print(f"{Fore.RED}Error copying dataset:{Style.RESET_ALL} {e}")
 
+else:
+    print(
+        f"{Fore.YELLOW}Warning:{Style.RESET_ALL} 'data/training_data' directory already exists! Delete it if you want to copy the dataset again.")
+
 # Check if the specific problematic file exists and rename it if necessary
-wrong_file_path = "data/BraTS20_Training_355/W39_1998.09.19_Segm.nii"
-corrected_file_path = "data/BraTS20_Training_355/BraTS20_Training_355_seg.nii"
+wrong_file_path = "data/training_data/BraTS20_Training_355/W39_1998.09.19_Segm.nii"
+corrected_file_path = "data/training_data/BraTS20_Training_355/BraTS20_Training_355_seg.nii"
 
 if os.path.exists(wrong_file_path):
     try:
